7dedf1acdb8a62c626ce097b28d31e65f1b86a6a
IvanNavarreteTepozan <12ivanavarrete@gmail.com>
2025-08-07 20:38:26 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import Login from "../Components/Pages/Login.jsx"
+import Rutas from "../Components/Routing/Rutas.jsx"
+function App() {
+  return(
+    <>
+    <Rutas/>
+    </>
+  );
+}
+
+export default App
